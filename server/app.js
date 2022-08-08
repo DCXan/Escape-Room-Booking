a903b91bedbf7f808bb230d86b3c27529d33db8d
DCXan <chen.david.1016@gmail.com>
2022-08-08 13:04:55 -0500

    set up MongoDB connection and .env file

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
 
+require("dotenv").config()
 app.use(express.json());
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.s4nxcwu.mongodb.net/?retryWrites=true&w=majority`, {
+  useNewUrlParser: true, useUnifiedTopology: true 
+}, (error) => {
+    if(error) {
+      console.log(error)
+    } else {
+      console.log("Successfully connected to MongoDB database")
+    }
+})
+
 
 app.listen(8000, () => {
   console.log("Server is running on PORT 8000");
-  res.render("Hello World");
 });
