f8b0990f37e7502f20fcbfa0d6aafe5180d0727a
Rina Mendoza <rinamendozaa@gmail.com>
2022-08-10 10:35:34 -0400

    dummy data is displaying on homepage

--- client/src/admin_components/Button.jsx
+++ client/src/admin_components/Button.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
+  return (
+    <button
+      type="button"
+      style={{ bgColor, color, size, text, borderRadius }}
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
