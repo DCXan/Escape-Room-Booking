91dd223f35e6c93e837a661de9573a028a559e46
Rina Mendoza <rinamendozaa@gmail.com>
2022-08-14 16:33:48 -0400

    added a customer route for admin to see info

---
+++ server/schemas/customer.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const customerSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Customer = mongoose.model("Customer", customerSchema);
+module.exports = Customer;
