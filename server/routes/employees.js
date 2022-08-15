51cd538b4eef81da4ec5b22d076fa20f8fd1e6b8
Rina Mendoza <rinamendozaa@gmail.com>
2022-08-15 14:09:32 -0400

    set up jwt verification

--- server/routes/employees.js
+++ server/routes/employees.js
@@ -1,1 +1,40 @@
+import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/employee.js";
+import {
+  verifyAdmin,
+  verifyToken,
+  verifyUser,
+} from "../utils/tokenVerification";
 
+const router = express.employeeRouter();
+
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("hello user, you are logged in");
+});
+
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+  res.send("hello user, you are logged in and you can delete your account");
+});
+
+router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+  res.send("hello admin, you are logged in and you can delete all accounts");
+});
+
+//UPDATE
+router.put("/:id", verifyUser, updateUser);
+
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
+
+//GET
+router.get("/:id", verifyUser, getUser);
+
+//GET ALL
+router.get("/", verifyAdmin, getUsers);
+
+export default employeeRouter;
