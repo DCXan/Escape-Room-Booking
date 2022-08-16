2fa1cd896d3cde1bc69c0b3856238134afa09094
Rina Mendoza <rinamendozaa@gmail.com>
2022-08-16 16:58:36 -0400

    added media queries and height adjustments 2 modal

--- client/src/pages/RoomModal.jsx
+++ client/src/pages/RoomModal.jsx
@@ -1,18 +1,31 @@
-import React, { useState } from "react"
+import React, { useState, useRef, useEffect } from "react";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 
 const RoomModal = ({ room }) => {
-  const [showModal, setShowModal] = useState(false)
-  const [roomDetails, setRoomDetails] = useState({})
+  const [showModal, setShowModal] = useState(false);
+  const [roomDetails, setRoomDetails] = useState({});
+  const modalRef = useRef();
+
+  useEffect(() => {
+    const options = {
+      reserveScrollBarGap: true,
+    };
+    if (showModal) {
+      disableBodyScroll(modalRef, options);
+    } else {
+      enableBodyScroll(modalRef);
+    }
+  }, [showModal, modalRef]);
 
   const handleChange = (e) => {
     setRoomDetails({
       ...roomDetails,
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
-    <div>
+    <div className="Modal">
       <button
         className="bg-blue-500 text-white font-medium px-3 py-2 my-3 rounded-2xl hover:bg-blue-900 hover:drop-shadow-xl"
         onClick={() => setShowModal(true)}
@@ -21,10 +34,13 @@ const RoomModal = ({ room }) => {
       </button>
       {showModal ? (
         <div>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <div
+            ref={modalRef}
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mt-10 mb-10"
+          >
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full h-auto bg-white outline-none focus:outline-none top-80">
                 {/*header*/}
                 <button
                   className="p-1 ml-auto bg-transparent border-0 text-black opacity-25 float-right text-2xl leading-none font-semibold outline-none focus:outline-none hover:bg-gray-500 hover:rounded-xl mx-2 mt-2"
@@ -48,36 +64,79 @@ const RoomModal = ({ room }) => {
                 <div className="flex flex-col items-center justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <p className="text-xl">Room Name:</p>
                   <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="roomTitle" name="title" type="text" defaultValue={room.Subject} onChange={handleChange}/>
+                    <input
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
+                      id="roomTitle"
+                      name="title"
+                      type="text"
+                      defaultValue={room.Subject}
+                      onChange={handleChange}
+                    />
                   </div>
                   <p className="text-xl mt-3">Additional Details:</p>
                   <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="additionalDetails" name="additionalDetails" type="text" defaultValue={room.additionalDetails} onChange={handleChange}/>
+                    <input
+                      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
+                      id="additionalDetails"
+                      name="additionalDetails"
+                      type="text"
+                      defaultValue={room.additionalDetails}
+                      onChange={handleChange}
+                    />
                   </div>
                 </div>
-                <img src={room.image} />
+                <img src={room.image} alt="Escape Room" />
                 {/*Room Description*/}
                 <div className="flex flex-col items-center justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                    <p className="text-xl">Room Description:</p>
-                    <div className="md:w-2/3 self-center">
-                      {/* <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="additionalDetails" name="additionalDetails" type="textarea" defaultValue={room.description} onChange={handleChange}/> */}
-                      <textarea className="self-center mt-5" rows={8} cols={60} name="description" onChange={handleChange}>
+                  <p className="text-xl">Room Description:</p>
+                  <div className="md:w-2/3 self-center">
+                    {/* <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="additionalDetails" name="additionalDetails" type="textarea" defaultValue={room.description} onChange={handleChange}/> */}
+                    <textarea
+                      className="self-center mt-5"
+                      rows={4}
+                      cols={50}
+                      name="description"
+                      onChange={handleChange}
+                    >
                       {room.description}
-                      </textarea>
-                    </div>
+                    </textarea>
+                  </div>
                 </div>
                 <div className="flex flex-col justify-start p-6 border-t border-solid border-slate-200 rounded-b text-left">
-                  <p className="font-bold text-lg self-center">Additional Details</p>
+                  <p className="font-bold text-lg self-center">
+                    Additional Details
+                  </p>
                   <div className="self-center text-center">
                     <p className="content-between">
                       Adult Ticket Price: $
-                      <input className="md:w-1/3 m-2 self-end bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="adultRate" name="adultRate" type="text" defaultValue={room.adultRate} onChange={handleChange}/>
+                      <input
+                        className="md:w-1/3 m-2 self-end bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
+                        id="adultRate"
+                        name="adultRate"
+                        type="text"
+                        defaultValue={room.adultRate}
+                        onChange={handleChange}
+                      />
                     </p>
-                      Child Ticket Price: $
-                      <input className="md:w-1/3 m-2 self-end bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="achildRate" name="childRate" type="text" defaultValue={room.childRate} onChange={handleChange}/>
+                    Child Ticket Price: $
+                    <input
+                      className="md:w-1/3 m-2 self-end bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
+                      id="achildRate"
+                      name="childRate"
+                      type="text"
+                      defaultValue={room.childRate}
+                      onChange={handleChange}
+                    />
                     <p>
                       Private Room Rate: $
-                      <input className="md:w-1/3 m-2 self-end bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="achildRate" name="childRate" type="text" defaultValue={room.privateRate} onChange={handleChange}/>
+                      <input
+                        className="md:w-1/3 m-2 self-end bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
+                        id="achildRate"
+                        name="childRate"
+                        type="text"
+                        defaultValue={room.privateRate}
+                        onChange={handleChange}
+                      />
                     </p>
                   </div>
                 </div>
@@ -105,7 +164,7 @@ const RoomModal = ({ room }) => {
         </div>
       ) : null}
     </div>
-  )
-}
+  );
+};
 
-export default RoomModal
+export default RoomModal;
