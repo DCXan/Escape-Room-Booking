59a5454c26cce5e223db6c147b972b8db96300a8
Calvin Nguyen <calvin2nguyen@gmail.com>
2022-08-17 07:08:59 -0700

    customer info

--- customer/src/components/Calendar.jsx
+++ customer/src/components/Calendar.jsx
@@ -1,25 +1,212 @@
 import Calendar from "react-calendar"
 import "../calendar.css"
+import { loadStripe } from "@stripe/stripe-js"
 import React, { useState } from "react"
+import "react-phone-input-2/lib/style.css"
+import PhoneInput from "react-phone-input-2"
 import moment from "moment"
+import TextField from "@mui/material/TextField"
 
-function Booking({}) {
+//public key for stripe
+const stripePromise = loadStripe("pk_test_fmwCa9Gs1HrmcSrEAjsAvKQO00KtWSZf8C")
+moment.locale()
+const Booking = ({ room }) => {
+  const [showModal, setShowModal] = useState(false)
   const [date, setDate] = useState(new Date())
   const [showMyModal, setShowMyModal] = useState(false)
-  const [slots, setslots] = useState(false)
+  const [ticket, setTicket] = useState(false)
+  const [adult, setAdult] = useState({})
+  const [children, setChildren] = useState({})
+  const info = ["Adult", "Child", "Private Room"]
+
+  const handleBooking = async () => {}
+  const handleAdult = e => {
+    setAdult(
+      {
+        adult: e.target.value,
+      },
+      setChildren({
+        children: e.target.value,
+      })
+    )
+    if (adult <= room.maxPlayers) {
+    }
+  }
+  const handleChildren = e => {
+    setTicket({
+      children: e.target.value,
+    })
+  }
+  const handleTicket = () => {
+    console.log(ticket)
+  }
   const handleSlots = value => {
     setDate(value)
   }
+  const handleCheckout = () => {}
 
   return (
-    <div>
-      <div className="">
-        <Calendar minDetail="month" onClickDay={value => handleSlots(value)} />
-      </div>
-      <p className="text-center">
-        <span className="bold">Selected Date:</span> {date.toDateString()}
-      </p>
-    </div>
+    <>
+      <button
+        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Book Now
+      </button>
+      {showModal ? (
+        <>
+          <div className=" backdrop-blur-sm bg-white/30 justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto w-5/12 h-3/4">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="text-3xl font-semibold   border-solid border-slate-200 rounded-t">
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-6 w-6"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </span>
+                  </button>
+                </div>
+
+                <div className=" relative p-6 flex-auto">
+                  <div className="grid grid-cols-2  0 divide-x">
+                    <div className="justify-self-center">
+                      <Calendar
+                        minDetail="month"
+                        onClickDay={value => handleSlots(value)}
+                      />
+                    </div>
+                    <div>
+                      <p className=" text-2xl font-semibold ">
+                        {date.toDateString()}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="grid grid-cols-2 divide-x border-t border-solid border-slate-200 ">
+                    <div className="flex-row ">
+                      <div className="text-2xl">
+                        <p>Customer info</p>
+                      </div>
+                      <div className="flex-col ">
+                        <TextField
+                          size="small"
+                          label="First Name"
+                          placeholder="Enter first name"
+                          variant="outlined"
+                          required
+                        />
+                        <TextField
+                          size="small"
+                          label="Last Name"
+                          placeholder="Enter last name"
+                          variant="outlined"
+                          required
+                        />
+
+                        <TextField
+                          size="small"
+                          type="email"
+                          label="Email"
+                          placeholder="Enter email"
+                          variant="outlined"
+                          required
+                        />
+                        <PhoneInput InputProps={{ label: "email" }} />
+                      </div>
+                    </div>
+                    <p>Booking</p>
+                  </div>
+                </div>
+
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={handleCheckout}
+                  >
+                    Checkout
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
   )
 }
 export default Booking
+
+// const [date, setDate] = useState(new Date())
+// const [showMyModal, setShowMyModal] = useState(false)
+// const [slots, setslots] = useState(false)
+
+//   return (
+//     <div>
+//       <div className="">
+//         <Calendar minDetail="month" onClickDay={value => handleSlots(value)} />
+//       </div>
+//       <p className="text-center">
+//         <span className="bold">Selected Date:</span> {date.toDateString()}
+//       </p>
+//     </div>
+//   )
+// }
+
+// const handleCheckout = async () => {
+//   const line_items = [
+//     {
+//       price_data: {
+//         currency: "usd",
+//         product_data: {
+//           name: room.Subject,
+//           description: "Adult",
+//         },
+//         unit_amount: room.adultRate * 100,
+//       },
+//       quantity: room.maxPlayers,
+//     },
+//   ]
+//   const response = await fetch(
+//     "http://localhost:8000/checkout/create-checkout-session",
+//     {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ room }),
+//     }
+//   )
+//   const results = await response.json()
+//   if (results.success) {
+//     const stripe = await stripePromise
+//     stripe.redirectToCheckout({
+//       sessionId: results.sessionID,
+//     })
+//   }
+//
--- customer/src/components/RoomModal.jsx
+++ customer/src/components/RoomModal.jsx
@@ -1,85 +1,22 @@
 import React, { useState } from "react"
-import { loadStripe } from "@stripe/stripe-js"
+
 import Booking from "./Calendar"
-//public key for stripe
-const stripePromise = loadStripe("pk_test_fmwCa9Gs1HrmcSrEAjsAvKQO00KtWSZf8C")
 
 const RoomModal = ({ room }) => {
   const [showModal, setShowModal] = useState(false)
-  const [ticket, setTicket] = useState({})
-  const [adult, setAdult] = useState({})
-  const [children, setChildren] = useState({})
-  const info = ["Adult", "Child", "Private Room"]
-
-  const handleCheckout = async () => {
-    const line_items = [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: room.Subject,
-            description: "Adult",
-          },
-          unit_amount: room.adultRate * 100,
-        },
-        quantity: room.maxPlayers,
-      },
-    ]
-    const response = await fetch(
-      "http://localhost:8000/checkout/create-checkout-session",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ room }),
-      }
-    )
-    const results = await response.json()
-    if (results.success) {
-      const stripe = await stripePromise
-      stripe.redirectToCheckout({
-        sessionId: results.sessionID,
-      })
-    }
-  }
-  const handleAdult = e => {
-    setAdult(
-      {
-        adult: e.target.value,
-      },
-      setChildren({
-        children: e.target.value,
-      })
-    )
-    if (adult <= room.maxPlayers) {
-    }
-  }
-  const handleChildren = e => {
-    setTicket({
-      children: e.target.value,
-    })
-  }
-  const handleTicket = () => {
-    console.log(ticket)
-  }
-  const handleRoom = () => {
-    setShowModal(true)
-    console.log(room.Subject)
-  }
 
   return (
     <div>
       <button
         className="bg-blue-500 text-white font-medium px-3 py-2 my-3 rounded-2xl hover:bg-blue-900 hover:drop-shadow-xl"
-        onClick={handleRoom}
+        onClick={() => setShowModal(true)}
       >
         Room Details
       </button>
       {showModal ? (
         <div>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mx-2">
-            <div className="relative w-auto my-6 mx-auto max-w-7xl">
+            <div className="relative w-auto my-6 mx-auto max-w-4xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
@@ -108,9 +45,9 @@ const RoomModal = ({ room }) => {
                   </h3>
                   <p>{room.additionalDetails}</p>
                 </div>
-                <div className="  grid grid-cols-2 divide-x ">
+                <div className="">
                   <div className="flex flex-col ">
-                    <img src={room.image} />
+                    <img src={room.image} className="w-10/12 self-center" />
                     {/*Room Description*/}
                     <div className="relative p-6 flex-auto">
                       <p className="my-4 text-slate-500 text-lg text-justify">
@@ -118,11 +55,9 @@ const RoomModal = ({ room }) => {
                       </p>
                     </div>
                   </div>
-                  <div className="flex-col items-center ">
-                    <Booking />
-                  </div>
+                  <div className="flex-col items-center "></div>
                 </div>
-                <div className=" grid grid-cols-2 divide-x">
+                <div>
                   <div className="flex flex-col justify-start p-6 border-t border-solid border-slate-200 rounded-b text-left">
                     <p className="font-bold text-lg">Additional Details</p>
                     <div>
@@ -138,39 +73,6 @@ const RoomModal = ({ room }) => {
                       </p>
                     </div>
                   </div>
-                  <div className="flex flex-col">
-                    <h2>Tickets</h2>
-
-                    <div className="flex flex-row ">
-                      <label>Adult</label>
-                      <div>
-                        <select onChange={handleAdult}>
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                          <option value="4">4</option>
-                          <option value="5">5</option>
-                          <option value="6">6</option>
-                          <option value="7">7</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="flex flex-row ">
-                      <label>Children</label>
-                      <div>
-                        <select onChange={handleChildren}>
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                          <option value="4">4</option>
-                          <option value="5">5</option>
-                          <option value="6">6</option>
-                          <option value="7">7</option>
-                        </select>
-                      </div>
-                      <button onClick={handleTicket}>Click me</button>
-                    </div>
-                  </div>
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
@@ -181,13 +83,7 @@ const RoomModal = ({ room }) => {
                   >
                     Go Back
                   </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={handleCheckout}
-                  >
-                    Checkout
-                  </button>
+                  <Booking room={room} />
                 </div>
               </div>
             </div>
