d192412fee55bf5b1326fde1271cf97aeb6fc46a
Rina Mendoza <rinamendozaa@gmail.com>
2022-08-19 22:19:22 -0400

    may need refactoring, working at the moment.

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -2,106 +2,113 @@ const express = require("express");
 const customerRouter = express.Router();
 const Customer = require("../schemas/Customer");
 const Room = require("../schemas/room");
+const CustomerController = require("../controllers/customers");
+const checkAuth = require("../middleware/check-auth");
+
+customerRouter.post(
+  "/new-customer-booking",
+  CustomerController.customer_customerBooking
+);
 
 //Route will display all rooms in database
-customerRouter.get("/get-rooms", async (req, res) => {
-  try {
-    const rooms = await Room.find({});
+// customerRouter.get("/get-rooms", async (req, res) => {
+//   try {
+//     const rooms = await Room.find({});
 
-    res.json({
-      success: true,
-      rooms: rooms,
-    });
-  } catch (error) {
-    res.json({
-      success: false,
-      message: error,
-    });
-    console.log(error);
-  }
-});
-
-customerRouter.post("/update-booking/:customerID", async (req, res) => {
-  const { customerID, roomID } = req.params.customerID;
-  console.log(req.params);
-  const { first_name, last_name, email, phone, dateAndTime } = req.body;
-
-  console.log(req.body);
-
-  try {
-    Room.findByIdAndUpdate(customerID, {
-      first_name: first_name,
-      last_name: last_name,
-      email: email,
-      phone: phone,
-      dateAndTime: dateAndTime,
-      roomID: roomID,
-    });
-    res.json({
-      success: true,
-    });
-  } catch (err) {
-    res.status(404).json({ success: false, message: "room not found" });
-  }
-});
-// Room.findByIdAndUpdate(
-//   customerID,
-//   {
-//     first_name: first_name,
-//     last_name: last_name,
-//     email: email,
-//     phone: phone,
-//     dateAndTime: dateAndTime,
-//   },
-//   (error, data) => {
-//     if (error) {
-//       console.log(error);
-//       res.json({
-//         success: false,
-//         message: "Unable to update customer.",
-//       });
-//     } else {
-//       res.json({
-//         success: true,
-//       });
-//     }
+//     res.json({
+//       success: true,
+//       rooms: rooms,
+//     });
+//   } catch (error) {
+//     res.json({
+//       success: false,
+//       message: error,
+//     });
+//     console.log(error);
 //   }
-// );
+// });
 
-//Route will find one customer based on their id
-customerRouter.get("/get-booked-room/:id", async (req, res) => {
-  const id = req.params.id;
-  try {
-    const rooms = await Customer.findById(id, {});
-    res.json({
-      success: true,
-      rooms: rooms,
-    });
-  } catch (error) {
-    res.json({
-      success: false,
-      message: error,
-    });
-    console.log(error);
-  }
-});
+// customerRouter.post("/update-booking/:customerID", async (req, res) => {
+//   const { customerID, roomID } = req.params.customerID;
+//   console.log(req.params);
+//   const { first_name, last_name, email, phone, dateAndTime } = req.body;
 
-//admin will receive ALL CUSTOMERS
-customerRouter.get("/get-customers", async (req, res) => {
-  try {
-    const customers = await Customer.find({});
+//   console.log(req.body);
 
-    res.json({
-      success: true,
-      customers: customers,
-    });
-  } catch (error) {
-    res.json({
-      success: false,
-      message: error,
-    });
-    console.log(error);
-  }
-});
+//   try {
+//     Room.findByIdAndUpdate(customerID, {
+//       first_name: first_name,
+//       last_name: last_name,
+//       email: email,
+//       phone: phone,
+//       dateAndTime: dateAndTime,
+//       roomID: roomID,
+//     });
+//     res.json({
+//       success: true,
+//     });
+//   } catch (err) {
+//     res.status(404).json({ success: false, message: "room not found" });
+//   }
+// });
+// // Room.findByIdAndUpdate(
+// //   customerID,
+// //   {
+// //     first_name: first_name,
+// //     last_name: last_name,
+// //     email: email,
+// //     phone: phone,
+// //     dateAndTime: dateAndTime,
+// //   },
+// //   (error, data) => {
+// //     if (error) {
+// //       console.log(error);
+// //       res.json({
+// //         success: false,
+// //         message: "Unable to update customer.",
+// //       });
+// //     } else {
+// //       res.json({
+// //         success: true,
+// //       });
+// //     }
+// //   }
+// // );
+
+// //Route will find one customer based on their id
+// customerRouter.get("/get-booked-room/:id", async (req, res) => {
+//   const id = req.params.id;
+//   try {
+//     const rooms = await Customer.findById(id, {});
+//     res.json({
+//       success: true,
+//       rooms: rooms,
+//     });
+//   } catch (error) {
+//     res.json({
+//       success: false,
+//       message: error,
+//     });
+//     console.log(error);
+//   }
+// });
+
+// //admin will receive ALL CUSTOMERS
+// customerRouter.get("/get-customers", async (req, res) => {
+//   try {
+//     const customers = await Customer.find({});
+
+//     res.json({
+//       success: true,
+//       customers: customers,
+//     });
+//   } catch (error) {
+//     res.json({
+//       success: false,
+//       message: error,
+//     });
+//     console.log(error);
+//   }
+// });
 
 module.exports = customerRouter;
--- server/schemas/Customer.js
+++ server/schemas/Customer.js
@@ -2,37 +2,38 @@ const mongoose = require("mongoose");
 
 const customerSchema = new mongoose.Schema(
   {
-    roomID: mongoose.SchemaTypes.ObjectId,
-    first_name: {
+    _id: mongoose.Schema.Types.ObjectId,
+    // roomID: mongoose.SchemaTypes.ObjectId,
+    name: {
       type: String,
       required: true,
     },
-    last_name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    phone: {
-      type: String,
-      required: true,
-    },
-    dateAndTime: {
-      type: String,
-      required: true,
-    },
+    // last_name: {
+    //   type: String,
+    //   required: true,
+    // },
+    // email: {
+    //   type: String,
+    //   required: true,
+    // },
+    // phone: {
+    //   type: String,
+    //   required: true,
+    // },
+    // dateAndTime: {
+    //   type: String,
+    //   required: true,
+    // },
     // numberOfPlayers: {
     //   type: Number,
     //   required: true,
     // },
-    room: [
-      {
-        type: mongoose.Schema.ObjectId,
-        ref: "Room",
-      },
-    ],
+    // room: [
+    //   {
+    //     type: mongoose.Schema.ObjectId,
+    //     ref: "Room",
+    //   },
+    // ],
   },
   { timestamps: true }
 );
