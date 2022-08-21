55b7f226372679605556747818e99ab60192bba5
Rina Mendoza <rinamendozaa@gmail.com>
2022-08-21 18:47:50 -0400

    deployment branch

--- server/app.js
+++ server/app.js
@@ -68,7 +68,7 @@ io.on("connection", (socket) => {
 
 const getApiAndEmit = (socket) => {
   const response = new Date();
-  // Emitting a new message. Will be consumed by the client
+  // Emitting a new message. Will be consumed by the client.
   socket.emit("FromAPI", response);
 };
 
