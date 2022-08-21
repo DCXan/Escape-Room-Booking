e084986cb35ca360eefbfdadc50702a3c26154db
Rina Mendoza <rinamendozaa@gmail.com>
2022-08-21 18:15:53 -0400

    deploying

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const User = require("../schemas/user");
+const User = require("../schemas/User");
 
 exports.user_signup = (req, res, next) => {
   User.find({ email: req.body.email })
