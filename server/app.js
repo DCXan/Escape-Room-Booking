59b535db131eac20b4b9a3f16a43e8f2cb5023ed
Rina Mendoza <rinamendozaa@gmail.com>
2022-08-21 18:33:03 -0400

    deploying

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 
-// Routers
+// Routes
 
 const adminRouter = require("./routes/admin");
 const userRouter = require("./routes/user");
