133d5a5577dfed9e258eac7e15440aca0959e666
Calvin Nguyen <calvin2nguyen@gmail.com>
2022-08-21 17:00:52 -0700

    stripe checkout

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -1,37 +1,37 @@
-const express = require("express");
-const customerRouter = express.Router();
-const Customer = require("../schemas/Customer");
-const Room = require("../schemas/room");
+const express = require("express")
+const customerRouter = express.Router()
+const Customer = require("../schemas/Customer")
+const Room = require("../schemas/room")
 
 // Route will display all rooms in database
 customerRouter.get("/get-rooms", async (req, res) => {
   try {
-    const rooms = await Room.find({});
+    const rooms = await Room.find({})
 
     res.json({
       success: true,
       rooms: rooms,
-    });
+    })
   } catch (error) {
     res.json({
       success: false,
       message: error,
-    });
-    console.log(error);
+    })
+    console.log(error)
   }
-});
+})
 
 //Create a new customer
 customerRouter.post("/customer-booking/:id", async (req, res) => {
-  const newCustomer = await Customer.create(req.body);
+  const newCustomer = await Customer.create(req.body)
 
   res.status(201).json({
     status: "success",
     data: {
       customer: newCustomer,
     },
-  });
-});
+  })
+})
 
 //Sends all customer orders with room attached
 
@@ -41,19 +41,19 @@ customerRouter.get("/get-customers", async (req, res) => {
       path: "rooms",
       select:
         "-EndTime -RecurrenceRule -StartTime -additionalDetails -adultRate -childRate -createdAt -date -description -durationMinutes -maxPlayers -privateRate -updatedAt -__v -_id",
-    });
+    })
     res.json({
       success: true,
       customers: customers,
-    });
+    })
   } catch (error) {
     res.json({
       success: false,
       message: error,
-    });
-    console.log(error);
+    })
+    console.log(error)
   }
-});
+})
 
 //sends only selected data
 customerRouter.get("/get-limited-customer-details", async (req, res) => {
@@ -73,18 +73,18 @@ customerRouter.get("/get-limited-customer-details", async (req, res) => {
       path: "rooms",
       select:
         "-EndTime -RecurrenceRule -StartTime -additionalDetails -adultRate -childRate -createdAt -date -description -durationMinutes -image -maxPlayers -privateRate -updatedAt -__v -_id",
-    });
+    })
     res.json({
       success: true,
       customers: customers,
-    });
+    })
   } catch (error) {
     res.json({
       success: false,
       message: error,
-    });
-    console.log(error);
+    })
+    console.log(error)
   }
-});
+})
 
-module.exports = customerRouter;
+module.exports = customerRouter
--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,46 +1,44 @@
-const express = require("express");
-const checkoutRouter = express.Router();
-
-const stripe = require("stripe")(
-  "sk_test_51F8bRHLOnCTYLxxk3gEoLoDvkGaz4UWPqs69bKiArbf306fHovYU1L7ExCLqwznB5ZrQmqiPxUebEgqAPSXQSeNb00n0L71bb3"
-);
-
+const express = require("express")
+const checkoutRouter = express.Router()
+const bodyParser = require("body-parser")
+const stripe = require("stripe")(process.env.STRIPE_SK)
+const buffer = require("micro")
 checkoutRouter.use((req, res, next) => {
-  if (req.originalUrl === "/webhook") {
-    next(); // Do nothing with the body because I need it in a raw state.
+  if (req.originalUrl === "/webhooks") {
+    next()
   } else {
-    express.json()(req, res, next); // ONLY do express.json() if the received request is NOT a WebHook from Stripe.
+    bodyParser.json()(req, res, next)
   }
-});
+})
+
 checkoutRouter.post(
   "/webhooks",
-  express.raw({ type: "application/json" }),
-
+  bodyParser.raw({ type: "application/json" }),
   async (req, res) => {
-    const sig = req.headers["stripe-signature"];
-    const payload = req.body;
-    console.log(sig);
-
+    const requestBuffer = await buffer(req)
+    const payload = requestBuffer.toString()
+    const sig = req.headers["stripe-signature"]
+    let event
     const endpointSecret =
-      "whsec_ca7b3023d8dda0b657dffc2c3723d6a1214e900768f4f45fd80e6327813ccc61";
+      "whsec_ca7b3023d8dda0b657dffc2c3723d6a1214e900768f4f45fd80e6327813ccc61"
 
-    let event;
     try {
-      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret)
     } catch (error) {
-      console.log(error);
+      console.log(error.message)
     }
-    if (event.type == "checkout.session.completed") {
-      console.log("asdd");
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object
+      console.log("asdf")
     }
   }
-);
+)
 
 // connect to stripe
 checkoutRouter.post("/payment", async (req, res) => {
-  const { line_items } = req.body;
+  const { line_items, userInfo } = req.body
 
-  const domainUrl = "http://localhost:3001";
+  const domainUrl = "http://localhost:3001"
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
@@ -53,20 +51,23 @@ checkoutRouter.post("/payment", async (req, res) => {
       phone_number_collection: {
         enabled: true,
       },
-    });
-    res.status(200).json({ success: true, sessionID: session.id });
+      metadata: {
+        firstName: userInfo.firstName,
+      },
+    })
+    res.status(200).json({ success: true, sessionID: session.id })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-});
+})
 
 checkoutRouter.get("/success", async (req, res) => {
-  const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
-  const customer = await stripe.customers.retrieve(session.customer);
+  const session = await stripe.checkout.sessions.retrieve(req.query.session_id)
+  const customer = await stripe.customers.retrieve(session.customer)
 
-  console.log(customer);
+  console.log(customer)
 
-  res.json({ customer: customer });
-});
+  res.json({ customer: customer })
+})
 
-module.exports = checkoutRouter;
+module.exports = checkoutRouter
